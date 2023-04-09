9682a1d8d5fa9754f82079f4b781086ccb2633a4
Cyril-Xd <54032334+Cyril-Xd@users.noreply.github.com>
2023-04-09 19:49:15 -0400

    Update Avatar.cs

--- AvatarSearcher/Avatar.cs
+++ AvatarSearcher/Avatar.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace AvatarSearcher
 {
@@ -9,21 +9,22 @@ namespace AvatarSearcher
 
     public class Avatar
     {
-        public string TimeDetected { get; set; }
-        public string AvatarID { get; set; }
-        public string AvatarName { get; set; }
-        public string AvatarDescription { get; set; }
-        public string AuthorName { get; set; }
-        public string AuthorID { get; set; }
-        public string PCAssetURL { get; set; }
-        public string QUESTAssetURL { get; set; }
-        public string ImageURL { get; set; }
-        public string ThumbnailURL { get; set; }
-        public string UnityVersion { get; set; }
-        public string Releasestatus { get; set; }
-        public string Tags { get; set; }
-        public string Pin { get; set; }
-        public string PinCode { get; set; }
-        public string Created { get; set; }
+        public string avatarId { get; set; }
+        public string avatarName { get; set; }
+        public string avatarDescription { get; set; }
+        public string authorId { get; set; }
+        public string authorName { get; set; }
+        public string imageUrl { get; set; }
+        public string thumbnailUrl { get; set; }
+        public string releaseStatus { get; set; }
+        public string unityVersion { get; set; }
+        public List<string> tags { get; set; }
+        public string recordCreated { get; set; }
+    }
+
+    public class Root
+    {
+        public Avatar avatar { get; set; }
+        public List<object> tags { get; set; }
     }
 }
